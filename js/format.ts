28e5cf8623f156d4cf0588046db59519e719fc5f
Nathan Sarang-Walters <nwalters512@gmail.com>
2025-04-08 11:54:51 -0700

    More formatting

--- js/format.ts
+++ js/format.ts
@@ -1,66 +1,66 @@
-import './wasm_exec.js';
+import './wasm_exec.js'
 
 export interface FormatOptions {
-  indent: number;
-  trailingNewline: boolean;
+    indent: number
+    trailingNewline: boolean
 }
 
 export const formatDockerfileContents = async (
-  fileContents: string,
-  options: FormatOptions,
-  getWasm: () => Promise<Buffer>,
+    fileContents: string,
+    options: FormatOptions,
+    getWasm: () => Promise<Buffer>,
 ) => {
-  const go = new Go(); // Defined in wasm_exec.js
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder();
+    const go = new Go() // Defined in wasm_exec.js
+    const encoder = new TextEncoder()
+    const decoder = new TextDecoder()
 
-  // get current working directory
-  const wasmBuffer = await getWasm();
-  const wasm = await WebAssembly.instantiate(wasmBuffer, go.importObject);
+    // get current working directory
+    const wasmBuffer = await getWasm()
+    const wasm = await WebAssembly.instantiate(wasmBuffer, go.importObject)
 
-  /**
-   * Do not await this promise, because it only resolves once the go main()
-   * function has exited. But we need the main function to stay alive to be
-   * able to call the `parse` and `print` function.
-   */
-  go.run(wasm.instance);
+    /**
+     * Do not await this promise, because it only resolves once the go main()
+     * function has exited. But we need the main function to stay alive to be
+     * able to call the `parse` and `print` function.
+     */
+    go.run(wasm.instance)
 
-  const { memory, malloc, free, formatBytes } = wasm.instance.exports as {
-    memory: WebAssembly.Memory;
-    malloc: (size: number) => number;
-    free: (pointer: number) => void;
-    formatBytes: (
-      pointer: number,
-      length: number,
-      indent: number,
-      trailingNewline: boolean,
-    ) => number;
-  };
+    const { memory, malloc, free, formatBytes } = wasm.instance.exports as {
+        memory: WebAssembly.Memory
+        malloc: (size: number) => number
+        free: (pointer: number) => void
+        formatBytes: (
+            pointer: number,
+            length: number,
+            indent: number,
+            trailingNewline: boolean,
+        ) => number
+    }
 
-  const fileBufferBytes = encoder.encode(fileContents);
-  const filePointer = malloc(fileBufferBytes.byteLength);
+    const fileBufferBytes = encoder.encode(fileContents)
+    const filePointer = malloc(fileBufferBytes.byteLength)
 
-  new Uint8Array(memory.buffer).set(fileBufferBytes, filePointer);
+    new Uint8Array(memory.buffer).set(fileBufferBytes, filePointer)
 
-  // Call formatBytes function from WebAssembly
-  const resultPointer = formatBytes(
-    filePointer,
-    fileBufferBytes.byteLength,
-    options.indent,
-    options.trailingNewline,
-  );
+    // Call formatBytes function from WebAssembly
+    const resultPointer = formatBytes(
+        filePointer,
+        fileBufferBytes.byteLength,
+        options.indent,
+        options.trailingNewline,
+    )
 
-  // Decode the result
-  const resultBytes = new Uint8Array(memory.buffer).subarray(resultPointer);
-  const end = resultBytes.indexOf(0);
-  const result = decoder.decode(resultBytes.subarray(0, end));
-  free(filePointer);
+    // Decode the result
+    const resultBytes = new Uint8Array(memory.buffer).subarray(resultPointer)
+    const end = resultBytes.indexOf(0)
+    const result = decoder.decode(resultBytes.subarray(0, end))
+    free(filePointer)
 
-  return result;
-};
+    return result
+}
 
 export const formatDockerfile = () => {
-  throw new Error(
-    '`formatDockerfile` is not implemented in the browser. Use `formatDockerfileContents` instead.',
-  );
-};
+    throw new Error(
+        '`formatDockerfile` is not implemented in the browser. Use `formatDockerfileContents` instead.',
+    )
+}
